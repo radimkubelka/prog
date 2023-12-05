0958bd85e759913bf23a063f7bdc1eff2b3db03c
Radim Kubelka <rakubelka@gmail.com>
2023-12-05 11:21:38 +0100

    classplayground

---
+++ ClassPlayground/ClassPlayground/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class BankAccount
+    {
+        public int accountNumber;
+        public string holderName;
+        public string currency;
+        public double balance;
+
+        public BankAccount(int accountNumber, string holderName, string currency, double balance)
+        {
+            Random rnd = new Random();
+            this.accountNumber = accountNumber;
+            this.holderName = holderName;
+            this.currency = currency;
+            this.balance = balance;
+        }
+
+        public double Deposit(double addMoney)
+        {
+            balance += addMoney;
+            return balance;
+        }
+
+        public double Withdraw(double withdrawMoney)
+        {
+            balance -= withdrawMoney;
+            return balance;
+        }
+
+        public void Transfer(double sendMoney, int accountnumber)
+        {
+            if (balance >= sendMoney)
+            {
+                balance -= sendMoney;
+                balance += sendMoney;
+            }
+            else
+            {
+                Console.WriteLine("nemáš dost peněz");
+                return;
+            }
+        }
+    }
+}
---
+++ ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        public int width;
+        public int height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public float calculateArea()
+        {
+            float area = width * height;
+            return area;
+        }
+        public float calculateAspectRatio()
+        {
+            float aspectRatio = (float)width / height;
+            return aspectRatio;
+        }
+        public bool containsPoint(int x, int y)
+        {
+            bool result = true;
+            if (x > width || y > height)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+    
+}
